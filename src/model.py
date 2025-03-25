03b075f83aa0d3f794b11e98e4598601d010fe53
Jonahbranding <146873802+Jonahbranding@users.noreply.github.com>
2025-03-25 09:47:25 -0400

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,6 @@
+# Module for defining the model
+
+# This module defines the class handicapModel according to the specifications in Maynard-Smith' paper.
+
+# Author: Jonah Branding
+# Date: March 2025
---
+++ src/simulation.py
@@ -0,0 +1,4 @@
+# This module runs the simulation per the specificaitons in 'model.py'
+
+# Author: Jonah Branding
+# Date: March 2025
